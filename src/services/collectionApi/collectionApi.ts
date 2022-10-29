1601758618ef2729e6d3e5ad0f8cfc2a9d87ac31
MrMeshak <94204153+MrMeshak@users.noreply.github.com>
2022-10-29 16:56:56 +1100

    Updated api url

--- src/services/collectionApi/collectionApi.ts
+++ src/services/collectionApi/collectionApi.ts
@@ -1,34 +1,31 @@
-import axios from "axios";
-import { ILibrary, ILibraryDetails } from "../../store/library/libraryModels";
+import axios from 'axios';
+import { ILibrary, ILibraryDetails } from '../../store/library/libraryModels';
 
 export const axiosCollection = axios.create({
-    //baseURL: "http://localhost:4000/api/",
-    baseURL: "https://stacks-app.herokuapp.com/api",
-    timeout: 4000
-})
+  //baseURL: "http://localhost:4000/api/",
+  baseURL: 'https://stacks-app.up.railway.app/api',
+  timeout: 4000
+});
 
 export interface IGetCollection {
-    collectionId: string;
+  collectionId: string;
 }
 
 export interface IAddLibraryToCollection {
-    collectionId: string;
-    library: ILibrary;
+  collectionId: string;
+  library: ILibrary;
 }
 
-export interface IUpdateLibraryDetails{
-    collectionId: string;
-    libraryId: string;
-    libraryDetails: {
-        id: string;
-        name: string;
-    };
+export interface IUpdateLibraryDetails {
+  collectionId: string;
+  libraryId: string;
+  libraryDetails: {
+    id: string;
+    name: string;
+  };
 }
 
-export interface IDeleteLibraryInCollection{
-    collectionId: string;
-    libraryId: string;
+export interface IDeleteLibraryInCollection {
+  collectionId: string;
+  libraryId: string;
 }
-
-
-
